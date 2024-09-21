c2952a2061839f6e928e72cc2d9b73031eda0c8b
eemanDigital <eemandigitalconcept@gamil.com>
2024-09-21 15:32:49 +0100

    made a res status code change

--- controllers/CaseReportController.js
+++ controllers/CaseReportController.js
@@ -36,7 +36,7 @@ exports.createReport = catchAsync(async (req, res, next) => {
 
   // Create a new report associated with the found case
   const report = await Report.create(req.body);
-  res.status(200).json({
+  res.status(201).json({
     data: {
       message: "success",
       result: report,
